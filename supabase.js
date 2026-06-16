ff63098733d387230d836532c7a7541d22d2f56b
worthless-V1 <nojan4465@gmail.com>
2026-06-16 23:41:11 +0330

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://zwlsydqvvlqwwkmhtcvu.supabase.co";
+const SUPABASE_KEY = "sb_publishable_i_7l9mXz0VuJsCrc-iqj7A_p-vN697D";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
